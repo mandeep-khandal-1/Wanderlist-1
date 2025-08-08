2ddea6fa665a4067661649fc2d6c210ca3658782
Mandeep Khandal <mandeepkhandal0@gmail.com>
2025-08-08 21:08:22 +0530

    add custom error handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlist";
 
@@ -49,11 +50,15 @@ app.get("/listings/:id", async (req, res) => {
   res.render("listings/show.ejs", { listing });
 });
 
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+//Create Route
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -85,10 +90,15 @@ app.delete("/listings/:id", async (req, res) => {
 //     location: "Mumbai,Maharastra",
 //     country: "India",
 //   });
+
 //   await sampleListing.save();
 //   console.log("sample was saved");
 //   res.send("succesfull");
 // });
+
+app.use((err, req, res, next) => {
+  res.send("Something went wrong");
+});
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
